d2cddeebf790806568fbeb988fc6401cd667f034
Aldair Neves <aldairneves608@gmail.com>
2023-10-01 19:32:45 -0300

    estudando vue 2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
+  <div id="app">
+    <!-- <HelloWorld :testando="meuNome" :showIdade="true"/>
+    <HelloWorld :testando="meuNome" :showIdade="false"/>
+    <HelloWorld :testando="meuNome" :showIdade="true"/> -->
+    
+
+  <div v-for="(cliente, index) in clientes" :key="cliente.id">
+    <h4>{{ index + 1 }}</h4>
+    <HelloWorld :testando="cliente"/>
+    <hr>
   <div>
-    <HelloWorld :testando="meuNome" />
+    <h2>Editar</h2>
+    <input type="text" v-model="cliente.nome">
+    <input type="text" v-model="cliente.email">
+  </div>
+  </div>
   </div>
 </template>
 
@@ -12,12 +26,40 @@ export default {
   name: 'App',
   data() {
     return {
-      meuNome: {
-        eu: "Aldair Neves da Silva",
-        estado: "Bahia",
-        pais: "Brasil"
+      // meuNome: {
+      //   eu: "Aldair Neves da Silva",
+      //   estado: "Bahia",
+      //   pais: "Brasil",
+      //   idade: 23
 
-      }
+      // },
+      clientes: [
+        {
+          id: 4,
+          nome: "Aldair",
+          email: "aldair@gamail.com",
+        },
+        {
+          id: 5,
+          nome: "fulano",
+          email: "fulano@gamail.com",
+        },
+        {
+          id: 6,
+          nome: "Alguem",
+          email: "alguem@gamail.com",
+        },
+        {
+          id: 7,
+          nome: "Alguem",
+          email: "alguem@gamail.com",
+        },
+        {
+          id: 8,
+          nome: "Alguem",
+          email: "alguem@gamail.com",
+        },
+      ]
 
     }
   },
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,10 @@
 <template>
-  <div id="dns">
+  <div :class="{'dns' : !isCliente, 'Cliente' : isCliente}">
     <h1>Inicio dos Estudos - Vue</h1>
-    <h2>Meu nome é {{ testando.eu }}, sou do estado da {{ testando.estado }} - {{ testando.pais }}</h2>
+    <h2>Meu nome é {{ testando.nome }}, sou do estado da {{ testando.email }} - {{ testando.pais }}</h2>
+    <p v-if="showIdade === true">Meu nome Verdadeiro é {{ testando.idade }}</p>
+    <p v-else>Eu escondir a idade!</p>
+    <hr>
     <div>
     </div>
   </div>
@@ -10,8 +13,14 @@
 <script>
 
 export default {
+  data() {
+    return {
+      isCliente: true
+    }
+  },
   props: {
     testando: Object,
+    showIdade: Boolean
   }
 
 }
@@ -19,12 +28,21 @@ export default {
 </script>
 
 <style scoped>
-#dns {
+.dns {
   background-color: #dadada;
   max-width: 500;
   height: 350;
   padding: 1%;
   text-align: center;
   margin-top: 2%;
+}
+.Cliente {
+  background-color: #338bff;
+  max-width: 500;
+  height: 350;
+  padding: 1%;
+  text-align: center;
+  margin-top: 2%;
+  color: #fff;
 }
 </style>
